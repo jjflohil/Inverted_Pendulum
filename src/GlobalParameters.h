9fd98a06a4b06a6e8fe5c6716f617c6bff9e7d17
jjflohil <jasperflohil@gmail.com>
2024-01-18 00:24:52 +0100

    Pendulum stabilization achieved

--- src/GlobalParameters.h
+++ src/GlobalParameters.h
@@ -54,6 +54,8 @@ uint32_t  t_serial   = 0;   // Current time effect
 uint16_t  ts_serial  = 50;   // Sample time effect
 uint32_t  t_movingAvg   = 0;   // Current time effect
 uint16_t  ts_movingAvg  = 15;   // Sample time effect
+uint32_t  t_rbs   = 0;   // Current time effect
+uint16_t  ts_rbs  = 200;   // Sample time effect
 
 //Function Parameters
 uint8_t ModeP = 0;
@@ -63,5 +65,82 @@ uint32_t RightColor[12];
 uint32_t TotalColor[23];
 
 
+//State Feedback Parameters
+float A_system[1];
+//Observer Parameters
 
 
+//State feedback LQR
+struct LQR5{
+    float Q[5][5];
+    float R;
+    float K[1][5];
+};
+//State space representation with 5 states, 2 outputs and 1 input
+struct StateSpace_5_2_1{
+    float A[5][5];
+    float B[1][5];
+    float C[5][2];
+    float D[1][2];
+};
+
+StateSpace_5_2_1 Observer = {{1,         0.005,  0,          -0.002      ,   0       ,
+                              0.0905,    1,      0.0763,     -0.9047     ,   0.0131  ,
+                              0,         0,      1,          0.0048      ,   0       ,
+                              0,         0,      0,          0.8984      ,   00032   ,
+                              0,         0,      -0.0036,    -34.6930    ,   0.3417  },
+                            
+                            {0		    ,-0.0029,   0,	    -0.0008,    	0.2370  },
+                            
+                            {1, 0, 0, 0, 0, 
+                             0, 0, 1, 0, 0},
+
+                            {0, 0}
+                             };
+   
+
+float MatMul(float Mat0In[], float Mat1In[], int Size0, int Size1){
+    float MatOut[Size0][Size1];
+
+    for(int i=0;i<Size0;i++){
+        MatOut[i][0] =+ Mat0In[i]*Mat0In[i+Size1]; 
+    }
+
+}
+//m(0, 0) = 3;
+//m(1, 0) = 2.5;
+//m(0, 1) = -1;
+//m(1, 1) = m(1, 0) + m(0, 1);
+//MatrixXd m = MatrixXd::Random(3, 3);
+//m = (m + MatrixXd::Constant(3, 3, 1.2)) * 50;
+
+//LQR5 LQR_UP
+/*
+//Q = diag([1 0.01 20000 2.2 0]);
+//R = 20*10^-9;
+//K_ul = dlqr(A_ou,B_ou,Q,R);
+//u = round(-K_ul*[theta ; x_o(2) ; q ; x_o(4) ; x_o(5)] + G*r);
+
+% State feedback LQR
+Q = diag([5 0.4 4000 2 0]);
+R = 20*10^-6;
+K_u = dlqr(A_ou,B_ou,Q,R);
+
+% Observer LQR
+Q = diag([1 0 1 0.1 0]);
+R = [10 0 ; 0 50];
+L_u = dlqr(A_ou',C_ou',Q,R)';
+
+% State feedback LQR down
+Q = diag([30 0.1 10000 0.6 0]);
+R = 20*10^-9;
+K_d = dlqr(A_od,B_od,Q,R);
+
+% Observer LQR down
+Q = diag([1 1 1 1 0]);
+R = [1 0 ; 0 10];
+L_d = dlqr(A_od',C_od',Q,R)';
+
+% Gain which sets DC gain to 1 (Only perfect in case the model is perfect)
+G =  1./(Du(2)-(Cu(2,:)-Du(2)*K_u)*inv(Au-Bu*K_u)*Bu);
+*/
--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,16 @@
 #include <ArduinoOTA.h>
 #include <WiFiUdp.h>
 #include <WiFiClient.h> 
-
 //Load global parameters
 #include <GlobalParameters.h>
 
+
+// setting PWM properties
+const int freq = 20000;
+const int motorChannel = 0;
+const int resolution = 8;
+
+float ctrl_unlimited = 0;
 // Create a task handler to be able to use processor 0 for separate tasking 
 TaskHandle_t Task0_Wifi;
 TaskHandle_t Task1_ControlSystem;
@@ -20,19 +26,23 @@ WiFiUDP UDP;
 IPAddress macbookIP(192,168,2,23);
 unsigned int localUdpPort = 4210;
 //Parameters
-const float pi = 3.1415;
+const float pi = 3.14159265359;
 
 struct component {
   float refPos;
   float refVel;
+  float refAcc;
   float position;
   float velocity;
+  float acceleration;
   float lastPosition;
   float lastVelocity;
   float positionFiltered;
   float velocityFiltered;
+  float accelerationFiltered;
   float e_pos;
   float e_vel;
+  float e_acc;
 };
 
 struct EncoderPulse {
@@ -44,7 +54,7 @@ struct motor {
   const uint8_t VoltPin;
   const uint8_t DirPin0;
   const uint8_t DirPin1;
-  int Volt;
+  uint32_t Volt;
   bool Direction;
 };
 
@@ -53,20 +63,22 @@ EncoderPulse Enc0_1 = {22, 0};
 EncoderPulse Enc1_0 = {32, 0};
 EncoderPulse Enc1_1 = {33, 0};
 
-component cart = {0,0,0,0,0};
-component pend = {0,0,0,0,0};
+component cart = {0,0,0,0,0,0,0};
+component pend = {0,0,0,0,0,0,0};
 
 motor mot = {26,18,19,0,false};
 
 //Time parameters
 long  tc = 0;
-int   ts = 10;
+int   ts = 1;
 float tr = 0;
 float errInt = 0;
+float PendErrInt = 0;
 float errPast = 0;
 //Temporary control parameters
 float ctrl = 0;
-
+float ctrl_cur = 0;
+float ctrl_past = 0;
 
 //Initialize functions
 void onDmxFrame(uint16_t , uint16_t , uint8_t , uint8_t*);
@@ -115,7 +127,9 @@ float lastV = 0;
 
 float Pgain = 0;
 float Dgain = 0;
+float Again = 0;
 float Igain = 0;
+float Xgain = 0;
 float LP = 1.0;
 float pendPosGain = 0.0;
 float pendVelGain = 0.0;
@@ -186,11 +200,23 @@ void Task0_WifiCode( void * pvParameters ){
           UDP.printf(",");
           UDP.printf(String(uint32_t(5000000+cart.velocityFiltered*1000)).c_str(),10);
           UDP.printf(",");
+          UDP.printf(String(uint32_t(5000000+cart.accelerationFiltered*1000)).c_str(),10);
+          UDP.printf(",");
           UDP.printf(String(uint32_t(5000000+pend.position*1000)).c_str(),10);
           UDP.printf(",");
           UDP.printf(String(uint32_t(5000000+pend.velocityFiltered*1000)).c_str(),10);
           UDP.printf(",");
-          UDP.printf(String(uint32_t(5000000+errInt*1000)).c_str(),10);
+          UDP.printf(String(uint32_t(5000000+ctrl*255*1000)).c_str(),10);
+          UDP.printf(",");
+          UDP.printf(String(uint32_t(5000000+Mode*1000)).c_str(),10);
+          UDP.printf(",");
+          UDP.printf(String(uint32_t(5000000+Pgain*1000)).c_str(),10);
+          UDP.printf(",");
+          UDP.printf(String(uint32_t(5000000+Igain*1000)).c_str(),10);
+          UDP.printf(",");
+          UDP.printf(String(uint32_t(5000000+Dgain*1000)).c_str(),10);
+          UDP.printf(",");
+          UDP.printf(String(uint32_t(5000000+Xgain*1000)).c_str(),10);
           UDP.endPacket();
         }
       }
@@ -205,11 +231,17 @@ void Task0_WifiCode( void * pvParameters ){
 
 void Task1_ControlSystemCode( void * pvParameters ){
 
+  // configure LED PWM functionalitites
+  ledcSetup(motorChannel, freq, resolution);
+  
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(mot.VoltPin, motorChannel);
+
   pinMode(Enc0_0.PIN, INPUT_PULLUP);
   pinMode(Enc0_1.PIN, INPUT_PULLUP);
   pinMode(Enc1_0.PIN, INPUT_PULLUP);
   pinMode(Enc1_1.PIN, INPUT_PULLUP);
-  pinMode(mot.VoltPin, OUTPUT);
+  //pinMode(mot.VoltPin, OUTPUT);
   pinMode(mot.DirPin0, OUTPUT);
   pinMode(mot.DirPin1, OUTPUT);
 	attachInterrupt(Enc0_0.PIN, isr00, CHANGE);
@@ -226,94 +258,165 @@ void Task1_ControlSystemCode( void * pvParameters ){
       //Calculate and lowpass filter cart position and velocity
       cart.lastPosition = cart.position;
       cart.lastVelocity = cart.velocity;
-      cart.position = float(Enc0_0.position)/5800*0.25-0.125; //[m]
-      cart.velocity = (cart.position-cart.lastPosition)*10;
-      cart.positionFiltered = cart.positionFiltered + 1.0 * (cart.position - cart.positionFiltered);
-      cart.velocityFiltered = cart.velocityFiltered + 0.2 * (cart.velocity - cart.velocityFiltered);
+      cart.position = float(Enc0_0.position)/(5800/0.25)-0.125; //[m]
+      cart.velocity = (cart.position-cart.lastPosition)*(1000/float(ts));
+      cart.acceleration = (cart.velocity-cart.lastVelocity)*(1000/float(ts));
+      cart.positionFiltered = cart.positionFiltered + 0.1 * (cart.position - cart.positionFiltered);
+      cart.velocityFiltered = cart.velocityFiltered + 0.1 * (cart.velocity - cart.velocityFiltered);
+      cart.accelerationFiltered = cart.accelerationFiltered + 0.1 * (cart.acceleration - cart.accelerationFiltered);
 
       pend.lastPosition = pend.position;
       pend.lastVelocity = pend.velocity;
-      pend.position = float(Enc1_0.position)/2400*2*pi;  //[rad]
-      pend.velocity = (pend.position-pend.lastPosition)*10;
-      pend.positionFiltered = pend.positionFiltered + 1.0 * (pend.position - pend.positionFiltered);
-      pend.velocityFiltered = pend.velocityFiltered + 0.8 * (pend.velocity - pend.velocityFiltered);
+      pend.position = float(Enc1_0.position)/(2400/(2*pi));  //[rad]
+      pend.velocity = (pend.position-pend.lastPosition)*(1000/float(ts));
+      pend.positionFiltered = pend.positionFiltered + 0.1 * (pend.position - pend.positionFiltered);
+      pend.velocityFiltered = pend.velocityFiltered + 0.1 * (pend.velocity - pend.velocityFiltered);
 
-      // Stationary
+      //Switch off
       if(Mode==0){
-        Pgain = 20.0;
-        Dgain = 0.1;
-        Igain = 5.0;
+        Pgain = 0;
+        Dgain = 0;
+        Igain = 0;
+        cart.refPos = -0.125;
+        cart.refVel = 0;
+      }
+      // Stationary
+      if(Mode==1){
+        Pgain = float(G)/5;//5.0;
+        Igain = float(B)/5;//0.0;
+        Dgain = float(W)/10;//0.0;
         cart.refPos = -0.125;
         cart.refVel = 0.0;
       }
       //step
-      else if(Mode==1){
-        Pgain = 10.0;
-        Igain = 10.0;
-        Dgain = 0.05;
+      else if(Mode==2){
+        Pgain = float(G)/5;//5.0;
+        Igain = float(B)/5;//0.0;
+        Dgain = float(W)/10;//0.0;
+        //Pgain = 11;
+        //Igain = 5.6;
+        //Dgain = 0.0;
         cart.refPos = 0.1*round(sin(2*pi*0.1*tr));
         cart.refVel = 0.0;
       }
       //sine
-      else if(Mode==2){
-        Pgain = 20.0;
-        Igain = 20;// 0..25
-        Dgain = 60.0;
+      else if(Mode==3){
+        Pgain = float(G)/3;//5.0;
+        Igain = float(B)/3;//0.0;
+        Dgain = float(W)/10;//0.0;
         cart.refPos = 0.1*sin(2*pi*0.1*tr);
-        cart.refVel = 0.1*0.1*cos(2*pi*0.1*tr);
+        cart.refVel = 0.1*cos(2*pi*0.1*tr);
       }
       //track user input
-      else if(Mode==3){
-        Pgain = 15.0;
-        Igain = 10.0; // 0..25
-        Dgain = 0.01;
-        cart.refPos = 0.24 * (R-127)/255;
+      else if(Mode==4){
+        Pgain = float(G)/3;//5.0;
+        Igain = float(B)/3;//0.0;
+        Dgain = float(W)/10;//0.0;
+        cart.refPos = 0.24 * (float(R)-127)/255;
         cart.refVel = 0.0;
       }
       //track pendulum down
-      else if(Mode==4){
-        pendPosGain = 1.0;
-        pendVelGain = 0.1;
-        Pgain = 7;
-        Dgain = 40;
-        Igain = 0.0;
-        cart.refPos =  pendPosGain * pend.position;
-        cart.refVel =  pendVelGain * pend.velocityFiltered;
+      else if(Mode==5){
+        //Pendulum reference to center cart
+        pend.refPos = cart.position;
+        pend.e_pos = pend.refPos - pend.position;
+
+        //Integrator
+        if(pend.position > -0.5 && pend.position < 0.5){
+          PendErrInt = PendErrInt + pend.e_pos * float(ts)/1000;
+          if(PendErrInt > 1){
+          PendErrInt = 1;
+          }
+          if(PendErrInt < -1){
+            PendErrInt = -1;
+          }
+          ctrl = -10.0 * pend.e_pos - 0.0 * PendErrInt - 0.0 * pend.velocityFiltered;
+        }
+        else{
+          ctrl = 0;
+        }
       }
       //track pendulum up
-      else if(Mode==5){
-        pendPosGain = 1.0;
-        pendVelGain = 0.1;
-        Pgain = 7;
-        Dgain = 40;
-        Igain = 0.0;
-        cart.refPos =  -1 * pendPosGain * (pend.position-pi);
-        cart.refVel = pendVelGain * pend.velocityFiltered;
+      else if(Mode==6){
+
+        //Control Parameters
+        Pgain = 10.0;
+        Igain = 150.0;
+        Dgain = 0.1;
+        Pgain = float(G)/3;
+        Igain = float(B)*2;
+        Dgain = float(W)/50;
+        Xgain = float(Vel)/50;
+
+        //Pendulum reference to center cart
+        pend.refPos =  Dgain * cart.position;
+        pend.e_pos = (pend.refPos - (pend.position-pi));
+
+        
+        //Integrator
+        if(pend.position > (pi-0.5) && pend.position < (pi+0.5)){
+          PendErrInt = PendErrInt + pend.e_pos * float(ts)/1000;
+          if(PendErrInt > 1){
+          PendErrInt = 1;
+          }
+          if(PendErrInt < -1){
+            PendErrInt = -1;
+          }
+          ctrl = Pgain * pend.e_pos + Igain * PendErrInt - Xgain * pend.velocityFiltered;
+        }
+        else{
+          ctrl = 0;
+        }
+        
+      }
+      //RBS
+      else if(Mode==7){
+        if(millis() >= (t_rbs+ts_rbs)){
+          t_rbs = millis();
+          float amp = float(random(100,220))/255;
+          float direction = float(random(0,2));
+          ts_rbs = random(10,90);
+          if(direction){
+            ctrl = amp;
+          }
+          else{
+            ctrl = -amp; 
+          }
+        }
+      }
+      else if(Mode==14){
+        Enc0_0.position = 0;
+        Enc0_1.position = 0;
+        Enc1_0.position = 0;
+        Enc1_1.position = 0;
       }
       else{
         Pgain = 0;
+        Igain = 0;
         Dgain = 0;
       }
 
       cart.e_pos = cart.refPos-cart.positionFiltered;
       cart.e_vel = cart.refVel-cart.velocityFiltered;
 
-      //Integral action only in small error band of +-3cm
-      if(cart.e_pos < 0.03 && cart.e_pos > -0.03){
-        errInt = errInt + cart.e_pos/10;
+      //Integral action only in small error band of +-1cm
+      if(cart.e_pos < 0.02 && cart.e_pos > -0.02){
+        errInt = errInt + cart.e_pos*float(ts)/1000;
       }
-      //Anti-windup
-      if((errInt > 0 && cart.e_pos < 0) || (errInt < 0 && cart.e_pos > 0)){
-        errInt = 0;
+      //Limit Integrator action
+      if(errInt > 0.5){
+        errInt = 0.5;
       }
-      if(errInt > 1.0){
-        errInt = 1.0;
+      if(errInt < -0.5){
+        errInt = -0.5;
+
       }
-      if(errInt < -1.0){
-        errInt = -1.0;
+      if(Mode<5){
+        ctrl = Pgain * cart.e_pos + Igain * errInt + Dgain * cart.e_vel;
       }
-      ctrl = Pgain * cart.e_pos + Igain * errInt + Dgain * cart.e_vel;
       
+      
+      ctrl_unlimited = float(ctrl * 255);
       
       if(ctrl > 0){
         mot.Volt = int(ctrl*255);
@@ -324,29 +427,37 @@ void Task1_ControlSystemCode( void * pvParameters ){
       else{
         mot.Volt = 0;
       }
+      
       if(mot.Volt>255){
         mot.Volt = 255;
       }
-      if(Mode>0 && ((cart.position > 0.1 && ctrl > 0) || (cart.position < -0.1 && ctrl < 0))){
+      if(Mode > 1 && ((cart.position > 0.11 && ctrl > 0) || (cart.position < -0.11 && ctrl < 0))){
         mot.Volt = 0;
       }
+      
       //friction compensation right
-      if(ctrl > 0 && mot.Volt > 0 && mot.Volt < 90){
-        mot.Volt = 90;
+      float FrStaticR = 150;
+      if(ctrl > 0 && mot.Volt > 0 && mot.Volt < FrStaticR){
+        mot.Volt = FrStaticR;
       }
       //friction compensation left
-      if(ctrl < 0 && mot.Volt > 0 && mot.Volt < 90){
-        mot.Volt = 90;
+      float FrStaticL = 155;
+      if(ctrl < 0 && mot.Volt > 0 && mot.Volt < FrStaticL){
+        mot.Volt = FrStaticL;
       }
+      
 
+      //Determine motor direction
       mot.Direction = (ctrl<=0.0);
 
-      if(1){
-      analogWrite(mot.VoltPin,mot.Volt);
+      if(Mode == 0){
+        ledcWrite(motorChannel,0);
+      }
+      else{
+        ledcWrite(motorChannel,mot.Volt);
       }
       digitalWrite(mot.DirPin0,mot.Direction);
       digitalWrite(mot.DirPin1,1-mot.Direction);
-      
     }
   }
 }
@@ -354,9 +465,6 @@ void Task1_ControlSystemCode( void * pvParameters ){
 void loop() {
   ;
 }
-
-
-
 
 //==============================================================================//
 //==============================================================================//
@@ -370,23 +478,19 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* d
   // set brightness of the whole strip 
   if(universe==2){
     Mode  = data[(ID-1)*4 + 0];
-    if(Mode!=13){
       R     = data[(ID-1)*4 + 1];
       G     = data[(ID-1)*4 + 2];
       B     = data[(ID-1)*4 + 3];
       W     = data[(ID-1)*4 + 4];
       Vel   = data[(ID-1)*4 + 5];
-    }
     
   }
   if(universe==3){
     Mode  = data[(ID-1)*4 + 0];
-    if(Mode!=13){
       R_1     = data[(ID-1)*4 + 1];
       G_1     = data[(ID-1)*4 + 2];
       B_1     = data[(ID-1)*4 + 3];
       W_1     = data[(ID-1)*4 + 4];
       Vel_1   = data[(ID-1)*4 + 5];
-    }
   }
 }
